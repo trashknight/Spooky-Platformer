28206f923715b7aee62fd427aa4bb21655e37be4
Sarah Kitten <59421998+sarahkitten@users.noreply.github.com>
2021-11-05 16:14:27 -0700

    Canvas and collision fixes

--- Assets/Scripts/Mechanics/KinematicObject.cs
+++ Assets/Scripts/Mechanics/KinematicObject.cs
@@ -155,39 +155,41 @@ namespace Platformer.Mechanics
                     var count = body.Cast(move, contactFilter, hitBuffer, distance + shellRadius);
                     for (var i = 0; i < count; i++)
                     {
-                        var currentNormal = hitBuffer[i].normal;
+                        if ((hitBuffer[i].collider.gameObject.tag != "Player") && (hitBuffer[i].collider.gameObject.tag != "NoUp")) {
+                            var currentNormal = hitBuffer[i].normal;
 
-                        //is this surface flat enough to land on?
-                        if (currentNormal.y > minGroundNormalY)
-                        {
-                            IsGrounded = true;
-                            // if moving up, change the groundNormal to new surface normal.
-                            if (yMovement)
+                            //is this surface flat enough to land on?
+                            if (currentNormal.y > minGroundNormalY)
                             {
-                                groundNormal = currentNormal;
-                                currentNormal.x = 0;
+                                IsGrounded = true;
+                                // if moving up, change the groundNormal to new surface normal.
+                                if (yMovement)
+                                {
+                                    groundNormal = currentNormal;
+                                    currentNormal.x = 0;
+                                }
                             }
-                        }
-                        if (IsGrounded)
-                        {
-                            //how much of our velocity aligns with surface normal?
-                            var projection = Vector2.Dot(velocity, currentNormal);
-                            if (projection < 0)
+                            if (IsGrounded)
                             {
-                                //slower velocity if moving against the normal (up a hill).
-                                velocity = velocity - projection * currentNormal;
+                                //how much of our velocity aligns with surface normal?
+                                var projection = Vector2.Dot(velocity, currentNormal);
+                                if (projection < 0)
+                                {
+                                    //slower velocity if moving against the normal (up a hill).
+                                    velocity = velocity - projection * currentNormal;
+                                }
                             }
+                            else
+                            {
+                                //We are airborne, but hit something, so cancel vertical up and horizontal velocity.
+                                // CHANGING TO TRY TO FIX PLATFORMING
+                                // velocity.x *= 0;
+                                // velocity.y = Mathf.Min(velocity.y, 0);
+                            }
+                            //remove shellDistance from actual move distance.
+                            var modifiedDistance = hitBuffer[i].distance - shellRadius;
+                            distance = modifiedDistance < distance ? modifiedDistance : distance;
                         }
-                        else
-                        {
-                            //We are airborne, but hit something, so cancel vertical up and horizontal velocity.
-                            // CHANGING TO TRY TO FIX PLATFORMING
-                            // velocity.x *= 0;
-                            // velocity.y = Mathf.Min(velocity.y, 0);
-                        }
-                        //remove shellDistance from actual move distance.
-                        var modifiedDistance = hitBuffer[i].distance - shellRadius;
-                        distance = modifiedDistance < distance ? modifiedDistance : distance;
                     }
                 }
                 body.position = body.position + move.normalized * distance;
