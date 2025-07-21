0becdc4466a67662cd6c839f0dae854fce99c23c
trashknight <wmkitten@gmail.com>
2025-07-20 18:22:04 -0700

    Bottle sound effect added.
    Now it thuds when it hits the ground.

---
+++ Assets/Scripts/BottleLandingSound.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class BottleLandingSound : MonoBehaviour
+{
+    public AudioClip landingClip;
+    private bool hasLanded = false;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (hasLanded) return;
+
+        if (collision.CompareTag("BottleLandingZone")) // Your landing zone needs this tag!
+        {
+            AudioSource.PlayClipAtPoint(landingClip, transform.position);
+            hasLanded = true;
+        }
+    }
+}
